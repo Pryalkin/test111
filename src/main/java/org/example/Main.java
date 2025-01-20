7a11f2c41673ed338fa28dcdee08806b6e73c950
Pralkin <pppryalkin@gmail.com>
2025-01-20 16:02:44 +0300

    commit dev444

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,5 +14,8 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("333");
         }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("444");
+        }
     }
 }
