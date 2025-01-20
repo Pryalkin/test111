854ea256515e66346594e1010250f82b79a23b7a
Pralkin <pppryalkin@gmail.com>
2025-01-20 16:03:42 +0300

    commit dev555_1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,17 +8,6 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("111");
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("222");
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("333");
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("444");
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("555");
-        }
+
     }
 }
