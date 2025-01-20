ed38113ebeae14a6e8a1ec1c33946b5bc13a22ce
Pralkin <pppryalkin@gmail.com>
2025-01-20 15:22:46 +0300

    commit dev3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,9 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("*");
         }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("(-_-)");
+        }
 
     }
 }
